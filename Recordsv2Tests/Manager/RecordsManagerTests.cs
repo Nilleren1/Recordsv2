fe977a8a6dedb29af04e4aefe44ab5bcbf6d2484
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2022-04-06 10:47:56 +0200

    Unit test for filtering

--- Recordsv2Tests/Manager/RecordsManagerTests.cs
+++ Recordsv2Tests/Manager/RecordsManagerTests.cs
@@ -19,17 +19,26 @@ namespace Recordsv2.Manager.Tests
         public void GetAllTest()
         {
             //Arrange
+            IEnumerable<Record> noFilterList;
             IEnumerable<Record> recordList;
-
+            string artist = "kid";
+            string year = "2011";
 
             //Act
-            recordList = _manager.GetAll();
+            recordList = _manager.GetAll(artist, year);
+            noFilterList = _manager.GetAll("", "");
 
             //Assert
             //Vi gør det på to måder for at vise de forskellige måder det kan gøres på.
-            Assert.IsNotNull(_manager.GetAll());
+            Assert.IsNotNull(_manager.GetAll(artist, year));
             Assert.IsNotNull(recordList);
+            Assert.IsNotNull(noFilterList);
 
+            Assert.AreEqual("Kidd", recordList.ElementAt(0).Artist);
+            Assert.AreEqual("2011", recordList.ElementAt(0).PublicationYear);
+            Assert.AreEqual(3, noFilterList.Count());
+            Assert.AreEqual("Elvis best Beats", noFilterList.ElementAt(0).Title);
+            
         }
     }
 }
